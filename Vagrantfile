96cd1a08956e9f0dac17ee58865302678f3233da
Smartguy <franz.alliod@viacesi.fr>
2020-04-09 18:43:42 +0200

    fixed size VM + new instructions + modify Vagrantfile

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,6 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "centos/8"
+  config.disksize.size = '50GB'
   config.vm.box_url = "http://cloud.centos.org/centos/8/x86_64/images/CentOS-8-Vagrant-8.1.1911-20200113.3.x86_64.vagrant-virtualbox.box"
   config.vm.hostname = "myhost"
   config.vm.network "private_network", ip: "192.168.50.10"
--- script.sh
+++ script.sh
@@ -4,8 +4,8 @@
 dnf update -y 
 
 # removing previous install of docker
-rm -rf /var/lib/docker
-dnf remove docker docker-ce docker-ce-cli containerd.io
+# rm -rf /var/lib/docker
+# dnf remove docker docker-ce docker-ce-cli containerd.io
 
 # installing GUI tools
 dnf groupinstall -y gnome-desktop
@@ -19,35 +19,42 @@ systemctl isolate graphical.target
 systemctl set-default graphical.target
 
 # to add docker repository
-dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+# dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 
 # to add latest docker-ce version
-dnf install docker-ce --nobest -y
+# dnf install docker-ce --nobest -y
 
 # installing tools
 dnf install -y device-mapper-persistent-data lvm2 nano
 
 # enabling and starting docker
-systemctl start docker
-systemctl enable docker
+# systemctl start docker
+# systemctl enable docker
 
 # installing ansible
-dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
-dnf install -y ansible
+# dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm -y
+# dnf install -y ansible
 
 # set keyboard to french
 localectl set-keymap fr
 
 # create Dockerfile and execute it so container is ready on startup
-touch /home/vagrant/Dockerfile
-echo "FROM debian:9 
-EXPOSE 22/tcp
-EXPOSE 80/tcp
-RUN apt-get update -y \\
-&& apt-get install -y nginx" >> /home/vagrant/Dockerfile
+# touch /home/vagrant/Dockerfile
+# echo "FROM debian:9 
+# EXPOSE 22/tcp
+# EXPOSE 80/tcp
+# RUN apt-get update -y \\
+# && apt-get install -y nginx" >> /home/vagrant/Dockerfile
 # we build dockerfile
-docker build -t nginx-projet . -f /home/vagrant/Dockerfile
+# docker build -t nginx-projet . -f /home/vagrant/Dockerfile
 # start container with port 80 mapped to local port 80 and restart it if it goes down
-docker run -dit --restart unless-stopped --name nginx-projet -p 80:80 -d nginx 
+# docker run -dit --restart unless-stopped --name nginx-projet -p 80:80 -d nginx 
+
+## install firefox IF software package can't find anything !! -- OR fix /etc/yum.repos.d/gitlab_gitlab-ce.repo by removing 2nd entry of "gpgkey" ##
+# dnf install wget 
+# wget -O- "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" | tar -jx -C /usr/local/
+# mv /usr/bin/firefox /usr/bin/backup_firefox
+# echo "exclude=firefox" >> /etc/dnf/dnf.conf
+# ln -s /usr/local/firefox/firefox /usr/bin/firefox
 
 reboot now
